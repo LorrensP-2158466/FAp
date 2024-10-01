3064c3a3c9a4553d50425eaef62eb7dd935dfb3c
LorrensP-2158466 <lorrens.pantelis@student.uhasselt.be>
2024-10-01 16:10:57 +0200

    refactor data exploration code

--- data_exploration.py
+++ data_exploration.py
@@ -14,61 +14,13 @@ class DataExplorer():
     # add that name to this list:
     choices = ["all", "default", "amt_publications", "average_amt_authors", "unique_authors","papers_per_author", "average_paper_per_author"]
 
-    dataset_path: str
+    df: pl.DataFrame
     def __init__(self, dataset_path: str) -> None:
-        self.dataset_path = dataset_path
-
-
-    def perform_all(self) -> list[ExplorerResult]:
-        pass
-
-    def perform_default(self) -> list[ExplorerResult]:
-        pass
-
-
-    def amt_publications(self, df: pl.DataFrame) -> int:
-        return df.height
+        self.df = self.__create_dataframe(dataset_path)
     
-    def unique_authors(self, df: pl.DataFrame) -> int:
+    def __create_dataframe(self, dataset_path: str) -> pl.DataFrame:
         return (
-            df
-            .select(pl.col("names").explode().alias("all_names"))
-            .unique("all_names")
-            .height
-        )
-
-    def average_amt_authors(self, df: pl.DataFrame) -> int:
-        df = df.with_columns(
-                pl.col("names").list.len().alias("len_names") 
-            )
-        return df["len_names"].mean()
-
-
-    def median_amt_authors(self) -> int:
-        pass
-
-
-    def papers_per_author(self, df: pl.DataFrame) -> int:
-        result = (df
-            .select(pl.col("names").explode().alias("names"))
-            .group_by("names")
-            .agg(pl.count().alias("count")))
-        print(result)
-        return 0
-    
-    def average_paper_per_author(self, df: pl.DataFrame) -> int:
-        unique_authors = self.unique_authors(df)
-        return df.height / unique_authors if unique_authors > 0 else 0
-    
-
-    def perform(self, functions: list[str]) -> list[ExplorerResult]:
-        if "all" in functions:
-            return self.perform_all()
-        if "default" in functions:
-            return self.perform_default()
-                # Use read_csv to read the file
-        df = (
-            pl.read_csv(self.dataset_path, 
+            pl.read_csv(dataset_path, 
                         has_header=False,  # No header in the file
                         new_columns=["names"],
                         separator=";",
@@ -78,24 +30,78 @@ class DataExplorer():
                 pl.col("names").str.split(",").alias("names")
             )
         )
-        results: list[ExplorerResult] = []
+    def set_dataset_path(self, dataset_path: str):
+        self.df = self.__create_dataframe(dataset_path)
 
-        for func in functions:
-            match func:
-                case "all" | "default": ()
-                case "amt_publications":
-                    results.append((func, self.amt_publications(df)))
-                case "average_amt_authors": 
-                    results.append((func, self.average_amt_authors(df)))
-                case "unique_authors":
-                    results.append((func, self.unique_authors(df)))
-                case "papers_per_author":
-                    results.append((func, self.papers_per_author(df)))
-                case "average_paper_per_author":
-                    results.append((func, self.average_paper_per_author(df)))
-                case _:
-                    print(f"Unknown data exploration method: {func}")
-        return results
+    def perform_all(self) -> list[ExplorerResult]:
+        return [ result for fun in self.choices if (result := self.hanlde_method(fun)) is not None ]
+
+    def perform_default(self) -> list[ExplorerResult]:
+        pass
+
+    def amt_publications(self) -> int:
+        return self.df.height
+    
+    def unique_authors(self) -> int:
+        return (
+            self.df
+            .select(pl.col("names").explode().alias("all_names"))
+            .unique("all_names")
+            .height
+        )
+
+    def average_amt_authors(self) -> int:
+        df = self.df.with_columns(
+                pl.col("names").list.len().alias("len_names") 
+            )
+        return df["len_names"].mean()
+
+
+    def median_amt_authors(self) -> int:
+        pass
+
+
+    def papers_per_author(self) -> int:
+        result = (
+            self.df
+            .select(pl.col("names").explode().alias("names"))
+            .group_by("names")
+            .agg(pl.count().alias("count"))
+            )
+        print(result)
+        return 0
+    
+    def average_paper_per_author(self) -> int:
+        unique_authors = self.unique_authors()
+        return self.df.height / unique_authors if unique_authors > 0 else 0
+    
+
+    def hanlde_method(self, fun: str) -> ExplorerResult | None:
+        match fun:
+            case "all" | "default":
+                return None  # or some default action
+            case "amt_publications":
+                return (fun, self.amt_publications())
+            case "average_amt_authors":
+                return (fun, self.average_amt_authors())
+            case "unique_authors":
+                return (fun, self.unique_authors())
+            case "papers_per_author":
+                return (fun, self.papers_per_author())
+            case "average_paper_per_author":
+                return (fun, self.average_paper_per_author())
+            case _:
+                print(f"Unknown data exploration method: {fun}")
+                return None
+
+    def perform(self, functions: list[str]) -> list[ExplorerResult]:
+        
+        if "all" in functions:
+            return self.perform_all()
+        if "default" in functions:
+            return self.perform_default()
+                # Use read_csv to read the file
+        return [result for fun in functions if (result := self.hanlde_method(fun)) is not None ]
         
 
     # TODO: implement different types of explorations
